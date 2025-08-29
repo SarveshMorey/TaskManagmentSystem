07ec19f3ba3a965a12faf44404ec31db2393f88d
Sarvesh Morey <89193786+SarveshMorey@users.noreply.github.com>
2025-08-29 15:41:00 +0530

    Delete springboot_tms directory

--- springboot_tms/src/main/java/com/wipro/springboot_tms/entity/Status.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.springboot_tms.entity;
-
-public enum Status {
-    PENDING,
-    IN_PROGRESS,
-    COMPLETED
-}
--- springboot_tms/src/main/java/com/wipro/springboot_tms/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.springboot_tms.repository;
-
-import com.wipro.springboot_tms.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-    List<Task> findByUserUserId(Integer userId);
-}
